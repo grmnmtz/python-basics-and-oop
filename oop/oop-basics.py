b9b98cfbd58ae2dc7fd4a0fd3277281903010f29
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-03-30 22:29:18 -0600

    fix identation from oop basics file

--- oop/oop-basics.py
+++ oop/oop-basics.py
@@ -10,16 +10,21 @@ What does object oriented means?
 
 Object oriented means functionally directed toward modeling objects.
 
-This is one of many techniques used for modeling complex systems. It is defined by describing a collection of interacting objects via their data and behavior.
+This is one of many techniques used for modeling complex systems. 
+It is defined by describing a collection of interacting objects via their data and behavior.
 
 
 ------- Object Oriented Analysis (Exploration) --------------
 
-Object Oriented Analysis (OOA) is the process of looking at a problem, system or task (that somebody wants to turn into an application) and identifying the objects and interactions between those objects. The analysis stage is all about what needs to be done.
+Object Oriented Analysis (OOA) is the process of looking at a problem, system or task 
+(that somebody wants to turn into an application) and identifying the objects and interactions between those objects.
+The analysis stage is all about what needs to be done.
 
 Output:
 
-The output of the analysis stage is a set of requirements. If we were to complete the analysis stage in one step, we would have turned a task, such as I need a website, into a set of requirements. As an example here or some requirements as to what a website visitor might need to do.
+The output of the analysis stage is a set of requirements. If we were to complete the analysis stage in one step, 
+we would have turned a task, such as I need a website, into a set of requirements. As an example here or some 
+requirements as to what a website visitor might need to do.
 
 Actions ----------------- Objects
 
@@ -35,11 +40,15 @@ interviewing customers, studying their processes, and eliminating possibilities.
 
 ------------------ Object Oriented Design ---------------------
 
-OOD is the process of converting such requirements into an implementation specification. The designer must name the objects, define the behaviors, and formally specify which objects can activate specific behaviors on other objects. The design stage is all about how things should be done.
+OOD is the process of converting such requirements into an implementation specification. 
+The designer must name the objects, define the behaviors, and formally specify which objects can activate 
+specific behaviors on other objects. The design stage is all about how things should be done.
 
 Output:
 
-The output of the design stage is an implementation specification. If we were to complete the design stage in a single step, we would have turned the requirements defined during object-oriented analysis into a set of classes and interfaces that could be implemented in (ideally) any object-oriented programming language.
+The output of the design stage is an implementation specification. If we were to complete the design stage 
+in a single step, we would have turned the requirements defined during object-oriented analysis into a set of 
+classes and interfaces that could be implemented in (ideally) any object-oriented programming language.
 
 
 ----------------- Object Oriented Programming ------------------
@@ -47,9 +56,11 @@ The output of the design stage is an implementation specification. If we were to
 Object-Oriented programming (OOP) is the process of converting
 this perfectly-defined design into a working program that does exactly what the CEO originally requested.
 
-In real world, this doesn't happen. We'll always find things that need further analysis while we're designing. When we're programming, we find features that need clarification in the design.
+In real world, this doesn't happen. We'll always find things that need further analysis while we're designing. 
+When we're programming, we find features that need clarification in the design.
 
-In iterative development, a small part of the task is modeled, designed and programmed, and then the program is reviewed and expaned to improve each feature and include new features in a series of short development cycles.
+In iterative development, a small part of the task is modeled, designed and programmed, and then the program is 
+reviewed and expaned to improve each feature and include new features in a series of short development cycles.
  
 '''
 
